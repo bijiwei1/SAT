4d2716636fe2899684909b697b17cf15a073757f
bijiwei1 <bibijijiweihch@gmail.com>
2019-01-31 18:45:02 -0800

    test kernel util file

---
+++ src/kernel_util.cpp
@@ -0,0 +1,69 @@
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define NUM_VARS 251
+#define BUF_CLS_SIZE 20
+
+void collect_buffer(int pos_cls[NUM_VARS][BUF_CLS_SIZE], int neg_cls[NUM_VARS][BUF_CLS_SIZE], 
+  int lit, int x, 
+  int *extra_cls, int *extra_lit, 
+  int* num_extra){
+    if (lit> 0){
+      if (pos_cls[lit][0] == EMPTY){
+        pos_cls[lit][0] = x; 
+      }else if (pos_cls[lit][1] == EMPTY){
+        pos_cls[lit][1] = x;
+      }else if (pos_cls[lit][2] == EMPTY){
+        pos_cls[lit][2] = x;
+      }else if (pos_cls[lit][3] == EMPTY){
+        pos_cls[lit][3] = x; 
+      }else if (pos_cls[lit][4] == EMPTY){
+        pos_cls[lit][4] = x; 
+      }else if (pos_cls[lit][5] == EMPTY){
+        pos_cls[lit][5] = x; 
+      }else if (pos_cls[lit][6] == EMPTY){
+        pos_cls[lit][6] = x; 
+      }else if (pos_cls[lit][7] == EMPTY){
+        pos_cls[lit][7] = x;
+      }else if (pos_cls[lit][8] == EMPTY){
+        pos_cls[lit][8] = x; 
+      }else if (pos_cls[lit][9] == EMPTY){
+        pos_cls[lit][9] = x; 
+      }else {
+        extra_cls[num_extra[0]]= x; 
+        extra_lit[num_extra[0]] = lit;
+        num_extra[0] ++; 
+      }
+    }else{
+      //assert(neg_cls[var][4]>0);
+      if (neg_cls[-lit][0] == EMPTY){
+        neg_cls[-lit][0] = x; 
+      }else if (neg_cls[-lit][1] == EMPTY){
+        neg_cls[-lit][1] = x; 
+      }else if (neg_cls[-lit][2] == EMPTY){
+        neg_cls[-lit][2] = x; 
+      }else if (neg_cls[-lit][3] == EMPTY){
+        neg_cls[-lit][3] = x; 
+      }else if (neg_cls[-lit][4] == EMPTY){
+        neg_cls[-lit][4] = x; 
+      }else if (neg_cls[-lit][5] == EMPTY){
+        neg_cls[-lit][5] = x; 
+      }else if (neg_cls[-lit][6] == EMPTY){
+        neg_cls[-lit][6] = x; 
+      }else if (neg_cls[-lit][7] == EMPTY){
+        neg_cls[-lit][7] = x;
+      }else if (neg_cls[-lit][8] == EMPTY){
+        neg_cls[-lit][8] = x; 
+      }else if (neg_cls[-lit][9] == EMPTY){
+        neg_cls[-lit][9] = x; 
+      }else {
+        extra_cls[num_extra[0]]= x; 
+        extra_lit[num_extra[0]] = lit;
+        num_extra[0] ++; 
+    }
+
+  }
+}
--- src/solver.cpp
+++ src/solver.cpp
@@ -7,7 +7,13 @@
 //using namespace std; 
 
 #define NUM_CLAUSES 1024
-#define NUM_VARS 250
+#define NUM_VARS 251
+
+#define BUF_CLS_SIZE 20
+#define BUF_DED_SIZE 10
+
+void collect_buffer(int pos_cls[NUM_VARS][BUF_CLS_SIZE], int neg_cls[NUM_VARS][BUF_CLS_SIZE], 
+
 
 #pragma ACCEL kernel
 void solver_kernel(
@@ -17,43 +23,108 @@ void solver_kernel(
 #pragma ACCEL interface variable=c2 bus_bitwidth=512 depth = 1065
 #pragma ACCEL interface variable=c3 bus_bitwidth=512 depth = 1065
 #pragma ACCEL interface variable=result depth=1 
-
+/************************ Variable Declaration **************************/
   int satisfiable; 
   int unsatisfiable = 0; 
+  //Table and buffers
   int local_clauses[NUM_CLAUSES][3];
-  char var_truth_table[NUM_VARS]; // T, F, U (Undef) 
+  char var_truth_table[NUM_VARS] = {'U'}; 
+  // T, F, U (Undef), f(assigned to T first), t(assigned to F first)
+  bool dec_ded_type[NUM_VARS] = {0}; // 0 - DEC, 1 - DED
+  int dec_lvl[NUM_VARS] = {0}
+  int buf_ded[BUF_DED_SIZE] = {-1};
+  
+  int pos_cls[BUF_CLS_SIZE] = {-1}; 
+  int neg_cls[BUF_CLS_SIZE] = {-1}; 
+  int extra_cls[extra_buf_size] = {0}; 
+  int extra_lit[extra_buf_size] = {0}; 
 
-  //initialize buffer  
-  #pragma ACCEL parallel
-  for (int x = 1; x < NUM_VARS; x++){
-    var_truth_table[x] = 'U';
-  }
-  var_truth_table[3] = 'T';
-  var_truth_table[42] = 'T';
-  var_truth_table[48] = 'T';
+  //Idx and ptr 
+  int new_var_idx = 1;
+  int curr_lvl = -1; 
+  int buf_ded_curr = -1;
+  int buf_ded_end = -1;
+  //Other variables
+  int state = DECISION; 
 
 
+/*************************** Loading Clauses ***************************/
   //Load data
   printf("Start to load data \n");
   for (int x = 0; x < NUM_CLAUSES; ++x) {
     local_clauses[x][0] = c1[x];
     local_clauses[x][1] = c2[x];
     local_clauses[x][2] = c3[x];
+
+    collect_buffer(pos_cls, neg_cls, c1[x], x, extra_cls, extra_lit, num_extra);
+    collect_buffer(pos_cls, neg_cls, c2[x], x, extra_cls, extra_lit, num_extra);
+    collect_buffer(pos_cls, neg_cls, c3[x], x, extra_cls, extra_lit, num_extra);
   }
 
-  #pragma ACCEL parallel flatten reduction=unsatisfiable factor=32
-  for (int x = 0; x < NUM_CLAUSES; x++){ 
-    int l0_unsat =  (local_clauses[x][0] > 0) ? var_truth_table[local_clauses[x][0]] == 'F' :   var_truth_table[-local_clauses[x][0]] == 'T';
-    int l1_unsat =  (local_clauses[x][1] > 0) ? var_truth_table[local_clauses[x][1]] == 'F' :   var_truth_table[-local_clauses[x][1]] == 'T';
-    int l2_unsat =  (local_clauses[x][2] > 0) ? var_truth_table[local_clauses[x][2]] == 'F' :   var_truth_table[-local_clauses[x][2]] == 'T';
-    unsatisfiable |= l0_unsat & l1_unsat & l2_unsat; 
-  }
+/********************************* FSM **********************************/
+  while (state != EXIT){
+    switch(state){
+      case DECISION: 
+        printf("State = DECISION; ");
 
-  satisfiable = ~unsatisfiable; 
+        while (new_var_idx < NUM_VARS){
+          //printf("Skip var %d\n", new_var_idx); 
+          if (var_truth_table[new_var_idx] != 'U'){
+            new_var_idx ++; 
+          }else{
+            break; 
+          }
+        } 
+        
+        if (new_var_idx == NUM_VARS){
+          state = SOLVED; 
+        }else {
+          state = PROP;
+          //printf("Decide Var(%d)\n", new_var_idx);
+          curr_lvl ++; 
+
+          if (pos_cls[new_var_idx][8] != -1){
+            var_truth_table[new_var_idx] = T;
+          }else {
+            var_truth_table[new_var_idx] = F;
+          }
+ 
+          dec_ded_type[new_var_idx] = 0; // DEC
+          dec_lvl[new_var_idx] = curr_lvl; 
+        }
+
+        prev_state = DECISION; 
+        break;
+      case PROP:
+        if (prev_state == DECISION || prev_state == BACKTRACK){
+          prop_var = new_var_idx;
+          //printf("Decision Var(%d): %d\n", prop_var, var_truth_table[prop_var]);
+        }else if (prev_state == DEDUCTION){
+          prop_var = abs(buf_ded[buf_ded_curr]);
+          buf_ded_curr ++; 
+          //printf("deduction Var(%d): %d\n", prop_var, var_truth_table[prop_var]);
+        }
+
+        /****************** Regular buffer *****************/
+        int l1[BUF_CLS_SIZE], l2[BUF_CLS_SIZE];
+
+/*
+        #pragma ACCEL parallel flatten 
+        for (int x = 0; x < BUF_CLS_SIZE; x++){
+          if ()
+        }
+        */ 
+        break; 
+
+    }//end of sw
+  }//end of while
+
+
+
+  satisfiable = unsatisfiable ? 0 : 1; 
 
   printf("SAT result: %d\n", satisfiable);
   printf("unSAT result: %d\n", unsatisfiable);
-
 
   result[0] = satisfiable;
 
