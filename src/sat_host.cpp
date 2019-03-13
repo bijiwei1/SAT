6a604d6fbdb7ffe55de3a3a3d23d849d34b7c2f4
bijiwei1 <bibijijiweihch@gmail.com>
2019-03-13 02:21:23 -0700

    back to parallel version

--- src/sat_host.cpp
+++ src/sat_host.cpp
@@ -13,14 +13,14 @@ using std::string;
 using std::to_string;
 using std::vector;
 
-void solver_kernel(int mode, int* data_in1, int* data_in2, 
-            uint64_t* data_out1, uint64_t* data_out2, uint64_t* data_out3, uint64_t* data_out4);
 
 // Util functions for host
 void read_clause_file(string filename, int *c1, int *c2, int *c3,  int *max_size, 
   const int num_var, const int num_clauses); 
 
 int find_parent_cls( uint64_t data_out4[2], uint64_t data_out5[2], int i);
+
+void solver_kernel(int* c1, int* c2, int* c3, int* result);
 
 int main(int argc, char **argv) {
 
@@ -31,18 +31,6 @@ int main(int argc, char **argv) {
   int *c1 = (int *)malloc(sizeof(int) * NUM_ORG_CLAUSES);
   int *c2 = (int *)malloc(sizeof(int) * NUM_ORG_CLAUSES);
   int *c3 = (int *)malloc(sizeof(int) * NUM_ORG_CLAUSES);
-
-  int *data_in1 = (int *)malloc(sizeof(int));
-  int *data_in2 = (int *)malloc(sizeof(int));
-  int *data_in3 = (int *)malloc(sizeof(int));
-
-  uint64_t *data_out1 = (uint64_t *)malloc(sizeof(int)*2);
-  uint64_t *data_out2 = (uint64_t *)malloc(sizeof(uint)*2);
-  uint64_t *data_out3 = (uint64_t *)malloc(sizeof(uint)*2);
-  uint64_t *data_out4 = (uint64_t *)malloc(sizeof(uint)*2);
-  uint64_t *data_out5 = (uint64_t *)malloc(sizeof(uint)*2);
-
-
 
   int *max_size = (int *)malloc(sizeof(int) * 1); 
  
@@ -73,191 +61,8 @@ int main(int argc, char **argv) {
     wf<<"TestCase: " << fileName <<endl;  
     wf << "Time (Read file) : " << readtime.count() <<endl;
 
-    vector<int> clauses[10000]; 
-    int clauses_idx[NUM_PE *NUM_CLAUSES_PE]; //Only variable idx, If no var, => 255 
 
-    int var_truth_table[NUM_VARS] = {U};
-    bool var_ismarked[NUM_VARS] ={0};
-    bool var_ischecked[NUM_VARS] = {0}; 
-    int var_parents_cls[NUM_VARS] = {-1}; 
-    //vector<int> var_parents[NUM_VARS];
-    //int dec_lvl[NUM_VARS] = {-1};
-    //int dec_var[BUF_DEC_LVL]= {0}; // Variable idx at each decision lvl, we assume at most 100 decision level
-    //uint8_t assigned_stack[NUM_VARS] = {IDLE_VAR}; 
-    vector<int> assigned_stack; // {var_idx, is_dec} 
-
-    int state = INIT; 
-    int new_var_idx = 1;
-    int mode = 0; 
-    int load_cls_num = 0;
-    int curr_var, curr_var_info;
-    int curr_lvl, back_lvl; 
-    int tmp; //Used for temporary value
-
-
-    for (int i = 0; i < NUM_ORG_CLAUSES; i++){
-      clauses[i].push_back(c1[i]);
-      clauses[i].push_back(c2[i]);
-      clauses[i].push_back(c3[i]);
-    }
-
-    while (state != EXIT){
-    switch(state){
-
-      case INIT: 
-        mode = INIT;
-        state = PROP; 
-        break; 
-      case LOAD: 
-        mode = LOAD; 
-        if (load_cls_num >= NUM_ORG_CLAUSES){
-          state = DECISION; 
-          printf("Finish loading clauses\n");
-        }else{
-          state = PROP;
-          data_in1[0] = (abs(c1[load_cls_num])) | (abs(c2[load_cls_num]) << 8) | (abs(c3[load_cls_num]) << 16);
-          data_in2[0] = (c1[load_cls_num] > 0 ? POS : NEG) |
-                       ((c2[load_cls_num] > 0 ? POS : NEG) << 1) | ((c3[load_cls_num] > 0 ? POS : NEG) << 2);
-        }
-        break;
-
-      case DECISION: 
-        printf("Decision \n");
-        mode = DECISION; 
-        while (var_truth_table[new_var_idx] == U && new_var_idx < NUM_VARS){
-          printf("Skip var %d(Value - %d)\n", new_var_idx, var_truth_table[new_var_idx]); 
-          new_var_idx ++; 
-        } 
-        
-        if (new_var_idx == NUM_VARS){
-          state = SOLVED; 
-        }else {
-          state = PROP;
-          curr_lvl ++; 
-          printf("Decide Var(%d) - at lvl %d\n", new_var_idx, curr_lvl);
-
-          var_truth_table[new_var_idx] = T; //assigne to T
-          //dec_lvl[new_var_idx] = curr_lvl; 
-          //dec_var[curr_lvl] = new_var_idx; 
-          //assigned_stack[num_assigned] = new_var_idx;
-          assigned_stack.push_back( new_var_idx<<1 & 1);
-          data_in1[0] = T | (new_var_idx << 1) | (curr_lvl << 9);
-        }
-        break;
-
-      case PROP: 
-      /*
-       *  data_in1(32), data_in2(32), 
-       *  data_out1(64)
-       *  INIT: data_in1 = {c3, c2, c1} // each 8 bit var idx, 
-       *        data_in2 = {s3, s2, s1} // each 1 bit sign
-       *        data_out1 = {cls_no, pe_no} // cls_no(10), pe_no(6)
-       *        
-       *  DEC : data_in1 = {curr_lvl, var_idx, s} // var_idx(8), sign(1), T/F
-       *        data_out1[0] = {conf_cls_no, conf_cls_pe, conf_var, conf} // conf_cls_no(10), conf_cls_pe(6), conf_var(8), conf(1)
-       *        data_out1[1] =  {num of ded, xxxx xxxx xxxx xxxx} //assigned value for the above data , num of ded variable
-       *        data_out2[0/1] = {var_idx*8} //Each 8 bits - total 8*2 variables
-       *        data_out3[0/1] = {parent_cls1*4} //Each parent_cls has cls_no(10) & pe_no(6)
-       *        data_out4[0/1] = {parent_cls1*4} //Each parent_cls has cls_no(10) & pe_no(6)
-       *  BACK: Same as above 
-       *        data_in2 = {back_lvl}        
-       */
-        printf("mode %d, data_in1: %#08x, data_in2: %#08x\n", data_in1, data_in2);
-        solver_kernel(mode, data_in1, data_in2, data_out1, data_out2, data_out3, data_out4);
-        printf("data_out1: %#016x %#016x, data_out2: %#016x\n ", data_out1[0], data_out1[1], data_out2[0], data_out2[1]);
-        printf("data_out3: %#016x %#016x, data_out4: %#016x\n ", data_out3[0], data_out3[1], data_out4[0], data_out4[1]);
-        if (mode == INIT){
-          printf("Finish initializing\n");
-          state = LOAD;
-        }else if (mode == LOAD){
-          state = LOAD; 
-          clauses_idx[data_out1[0]] = load_cls_num;
-          load_cls_num ++; 
-        }else if ((data_out1[0] & 1) == 1){
-          //Conflict
-          state = BACKTRACK;
-        }else{
-          //Not found conflict 
-          int var, parent_cls, global_cls;
-          int size = data_out1[1]>>16;
-
-          for (int i = 0; i <= size; i ++){
-            if (i < 8){
-              var = (data_out2[0] >> i*8) & 0xff;
-            }else{
-              var = (data_out2[1] >> i*8) & 0xff;
-            }
-            parent_cls = find_parent_cls(data_out4, data_out5, i);
-
-            var_truth_table[var] = (data_out1[1] >> i) & 0x1;
-            var_parents_cls[var] = parent_cls; 
-            global_cls = clauses_idx[parent_cls]; 
-          }
-          state = DECISION;
-        }
-        break;
-
-      case BACKTRACK: 
-        //printf("State = BACKTRACK_DEC; ");
-        back_lvl = curr_lvl; 
-        curr_var_info = assigned_stack.back(); 
-        assigned_stack.pop_back();
-        curr_var = curr_var_info >> 1; 
-        while(curr_var_info&0x1 == 0 || var_ischecked[curr_var]){
-          if (assigned_stack.empty()){
-            back_lvl = -1;
-            break;
-          }
-          //Undo assignment
-          var_truth_table[curr_var] = U;
-          var_ischecked[curr_var] = 0; 
-          curr_var_info = assigned_stack.back(); 
-          assigned_stack.pop_back();
-          curr_var = curr_var_info >> 1; 
-        }
-
-        if (back_lvl < 0){
-          printf("Failed at lvl %d\n", back_lvl);
-          state = FAILED; 
-          break;
-        }
-
-        new_var_idx = curr_var; 
-        printf("Back to lvl %d - Var %d\n", back_lvl, new_var_idx);
-        var_truth_table[new_var_idx] = (var_truth_table[new_var_idx] == T) ? F : T;
-        var_ischecked[new_var_idx] = 1; 
-        tmp = var_truth_table[new_var_idx] | (new_var_idx << 1);
-        assigned_stack.push_back(tmp);
-        printf("Change VTT Var(%d) to %d\n", new_var_idx, var_truth_table[new_var_idx]);
-        curr_lvl = back_lvl; 
-
-        state = PROP;
-        data_in1[0] = tmp;
-        data_in2[0] = curr_lvl; 
-        break;
-
-      case SOLVED:
-        printf("Solved\n");
-        for (int i = 0; i < NUM_ORG_CLAUSES; i++){
-            int lit1 = c1[i];
-            int lit2 = c2[i];
-            int lit3 = c3[i];
-            bool sat1 = (var_truth_table[abs(lit1)] == T && lit1 > 0) || (var_truth_table[abs(lit1)] == F && lit1 <0);
-            bool sat2 = (var_truth_table[abs(lit2)] == T && lit2 > 0) || (var_truth_table[abs(lit1)] == F && lit2 <0);
-            bool sat3 = (var_truth_table[abs(lit3)] == T && lit3 > 0) || (var_truth_table[abs(lit1)] == F && lit3 <0);
-            assert(sat1 || sat2 || sat3);
-        }
-        state = EXIT;
-        break; 
-
-      case FAILED:
-        printf("Failed to solve the problem. \n");
-        state = EXIT; 
-        break;
-    }//end of sw
-
-
-  }
+    solver_kernel(c1, c2, c3, result);
 
     auto ts3 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> readtime2 = ts3 -ts2; 
--- src/solver.cpp
+++ src/solver.cpp
@@ -7,43 +7,19 @@
 #include <config.h>
 #include <stdint.h>
 
-//using namespace std; 
-
-
-/*
-*  data_in1(32), data_in2(32), 
-*  data_out1(64)
-*  INIT: initial kernel
-*  LOAD: data_in1 = {c3, c2, c1} // each 8 bit var idx, 
-*        data_in2 = {s3, s2, s1} // each 1 bit sign
-*        data_out1 = {cls_no, pe_no} // cls_no(10), pe_no(6)
-*        
-*  DEC : data_in1 = {curr_lvl, var_idx, s} // var_idx(8), sign(1), T/F
-*        data_out1[0] = {conf_cls_no, conf_cls_pe, conf_var, conf} // conf_cls_no(10), conf_cls_pe(6), conf_var(8), conf(1)
-*        data_out1[1] =  {num of ded, xxxx xxxx xxxx xxxx} //assigned value for the above data , num of ded variable
-*        data_out2[0/1] = {var_idx*8} //Each 8 bits - total 8*2 variables
-*        data_out3[0/1] = {parent_cls1*4} //Each parent_cls has cls_no(10) & pe_no(6)
-*        data_out4[0/1] = {parent_cls1*4} //Each parent_cls has cls_no(10) & pe_no(6)
-*  BACK: Same as above   
-*        data_in2 = {back_lvl}       
-*/
 
 
 int priority_encoder_16(int in);
 int priority_encoder_64(uint64_t in);
 
 #pragma ACCEL kernel
-void solver_kernel(int mode, int* data_in1, int* data_in2, 
-            uint64_t* data_out1, uint64_t* data_out2, uint64_t* data_out3, uint64_t* data_out4){
+void solver_kernel(
+    int* c1, int* c2, int* c3, int* result){
 
-#pragma ACCEL interface variable=mode depth = 1
-#pragma ACCEL interface variable=data_in1 depth = 1
-#pragma ACCEL interface variable=data_in2 depth = 1
-#pragma ACCEL interface variable=data_out1 depth = 2
-#pragma ACCEL interface variable=data_out2 depth = 2
-#pragma ACCEL interface variable=data_out3 depth = 2
-#pragma ACCEL interface variable=data_out4 depth = 2
-#pragma ACCEL interface variable=data_out5 depth = 2
+#pragma ACCEL interface variable=c1 depth = 218
+#pragma ACCEL interface variable=c2 depth = 218
+#pragma ACCEL interface variable=c3 depth = 218
+#pragma ACCEL interface variable=result depth=1
 /************************ Variable Declaration **************************/
 
   int local_mode = mode; 
@@ -60,8 +36,17 @@ void solver_kernel(int mode, int* data_in1, int* data_in2,
 
 
   /* Variable Information */
-  uint8_t var_truth_table[NUM_VARS]; // T, F, U (Undef)
-  int var_dec_lvl[NUM_VARS]; 
+  uint8_t var_truth_table[NUM_VARS] = {U}; // T, F, U (Undef)
+  uint32_t var_ischecked[NUM_VARS] = 0;
+  bool var_ismarked[NUM_VARS] ={0};
+  int dec_lvl[NUM_VARS] = {-1};
+  int dec_var[BUF_DEC_LVL]= {0}; // Variable idx at each decision lvl, we assume at most 100 decision level
+  uint8_t assigned_stack[NUM_VARS] = {IDLE_VAR}; 
+  int num_assigned = 0; 
+  int buf_ded[BUF_DED_SIZE] = {0};
+  int buf_ded_cls[BUF_DED_SIZE] = {-1}; 
+  int parent_cls[NUM_VARS] = {0}; // 0 - 16: cls, > 17 : pe_no
+
 
   int buf_ded_lit[BUF_DED_SIZE];
   uint64_t buf_ded_parent_cls[BUF_DED_SIZE]; //pe: 0-5, cls: >6 (10bit)
@@ -86,36 +71,30 @@ void solver_kernel(int mode, int* data_in1, int* data_in2,
   uint8_t prop_var;
   int curr_lvl; 
 
-  int ii;
-  if (local_mode == INIT){
-    /*************************** Initializing array ***************************/
-    for (int i = 0; i < NUM_PE; i++){
-      clauses_cls_size[i] = 0;
-      for (int j = 0; j < NUM_VARS; j++){
-        for (int k = 0; k < NUM_P; k++){
-          pid_cls_info[i][j][k] = EMPTY;
-        }
+
+/*************************** Initializing array ***************************/
+  for (int i = 0; i < NUM_PE; i++){
+    clauses_cls_size[i] = 0;
+    for (int j = 0; j < NUM_VARS; j++){
+      for (int k = 0; k < NUM_P; k++){
+        pid_cls_info[i][j][k] = EMPTY;
       }
     }
+  }
 
-    for (int x = 0; x< NUM_VARS; x++){
-     var_truth_table[x] = U; 
-    }
 
-  }else if (local_mode == LOAD){
-    /*************************** Loading Clauses ***************************/
-    //Load data
-    printf("Start to load data \n");
-    //Load original clauses 
+/*************************** Loading Clauses ***************************/
+  //Load data
+  printf("Start to load data \n");
+  //Load original clauses 
     
-    int var1 = data_in1[0] & 0xff;
-    int var2 = data_in1[0] >> 8 & 0xff;
-    int var3 = data_in1[0] >> 16 & 0xff;
-    int s1 = data_in2[0] & 0x1; 
-    int s2 = data_in2[0]>>1 & 0x1; 
-    int s3 = data_in2[0]>>2 & 0x1; 
+  for (int i = 0; i < NUM_VARS; i++){
+    int var1 = abs(c1[0]);
+    int var2 = abs(c2[0]);
+    int var3 = abs(c3[0]);
     int avail_pe;
     int avail_idx1, avail_idx2;
+  
    
     for (avail_pe = 0; avail_pe < NUM_PE; avail_pe ++){
       avail_idx1 = (pid_cls_info[avail_pe][var1][0] == EMPTY) ? 0 : 
@@ -137,7 +116,7 @@ void solver_kernel(int mode, int* data_in1, int* data_in2,
     clauses[avail_pe][clauses_cls_size[avail_pe]][0] = var1;
     clauses[avail_pe][clauses_cls_size[avail_pe]][1] = var2;
     clauses[avail_pe][clauses_cls_size[avail_pe]][2] = var3;
-    clauses_sign[avail_pe][cls_idx] = data_in2[0];
+    clauses_sign[avail_pe][cls_idx] = (c1[i] > 0 ? T : F ) | ((c2[i] > 0 ? T : F) << 1) | ((c2[i] > 0 ? T : F) << 2);
 
     //Write variable info
     watch_var_info[avail_pe][cls_idx][0] = (var1 << 5);
@@ -146,9 +125,7 @@ void solver_kernel(int mode, int* data_in1, int* data_in2,
     pid_cls_info[avail_pe][var2][avail_idx2] = clauses_cls_size[avail_pe];
     clauses_cls_size[avail_pe] ++; 
 
-    data_out1[0] = avail_pe | cls_idx << 6;
-    printf("Add cls %d %d %d to pe %d idx %d\n", var1, var2, var3, avail_pe, clauses_cls_size[avail_pe]);
-
+  }
     /*
   for (int i = 0; i < NUM_PE; i++){
     for (int j = 0; j < clauses_cls_size[i]; j++){
@@ -156,27 +133,51 @@ void solver_kernel(int mode, int* data_in1, int* data_in2,
     }
   } */
 
-  }else if (mode == DECISION || mode == BACKTRACK){
 
-    prop_var = data_in1[0] >> 1 & 0xff;
-    var_truth_table[prop_var] = data_in1[0] & 0x1; 
-    curr_lvl = data_in1[0] >> 9;
+/********************************* FSM **********************************/
+  while (state != EXIT){
+  switch(state){
+    case DECISION: 
 
-    if (mode == BACKTRACK){
-      for (int i = 0; i < NUM_VARS; i++){
-        if(var_dec_lvl[i] >= curr_lvl){
-          var_truth_table[i] = U;
-          var_dec_lvl[i] = -1;
+      while (new_var_idx < NUM_VARS){
+        if (var_truth_table[new_var_idx] != U){
+          //printf("Skip var %d(Value - %d)\n", new_var_idx, var_truth_table[new_var_idx]); 
+          new_var_idx ++; 
+        }else{
+          break; 
         }
-      }
-    }
+      } 
+        
+      if (new_var_idx == NUM_VARS){
+        state = SOLVED; 
+      }else {
+        state = PROP;
+        curr_lvl ++; 
+        //printf("Decide Var(%d) - at lvl %d\n", new_var_idx, curr_lvl);
 
-    bool isdone = 0;
-    while (~isdone){
-    printf("Prop var %d\n", prop_var);
-    #pragma ACCEL parallel flatten
-    for (int pe_no = 0; pe_no < NUM_PE; pe_no++){
-      for (int i = 0; i < NUM_P; i++){
+        var_truth_table[new_var_idx] = T;
+        dec_lvl[new_var_idx] = curr_lvl; 
+        dec_var[curr_lvl] = new_var_idx; 
+        least_parent[new_var_idx] = new_var_idx;
+      }
+
+      prev_state = DECISION; 
+      break;
+
+    case PROP: 
+      if (prev_state == DECISION || prev_state == BACKTRACK_DEC){
+        prop_var = new_var_idx;
+      }else if (prev_state == DEDUCTION){
+        prop_var = buf_ded_lit[buf_curr]; 
+      }else if (prev_state == BACKTRACK_DED){
+        prop_var = abs(learned_lit);
+      }
+
+
+      printf("Prop var %d\n", prop_var);
+      #pragma ACCEL parallel flatten
+      for (int pe_no = 0; pe_no < NUM_PE; pe_no++){
+        for (int i = 0; i < NUM_P; i++){
         conflict[pe_no][i] = 0;
         cls_ded[pe_no][i] = -1;
         int cls_info = pid_cls_info[pe_no][prop_var][i];
@@ -222,150 +223,140 @@ void solver_kernel(int mode, int* data_in1, int* data_in2,
           } 
         }
 
-      }//End of p
-    }//End of pe
+        }//End of p
+      }//End of pe
 
-    //Inference collection
-    tot_conflict[0] = 0; 
-    tot_conflict[1] = 0; 
-    tot_conflict[2] = 0; 
-    tot_conflict[3] = 0; 
-    #pragma ACCEL parallel flatten reduction=tot_conflict
-    for (int i = 0; i < NUM_PE ; i++){
-      for (int j = 0; j < NUM_P; j++){
-        tot_conflict[j] |= (cls_ded[i][j] == -1 ? 0 : 1<<i); 
+      //Inference collection
+      tot_conflict[0] = 0; 
+      tot_conflict[1] = 0; 
+      tot_conflict[2] = 0; 
+      tot_conflict[3] = 0; 
+      #pragma ACCEL parallel flatten reduction=tot_conflict
+      for (int i = 0; i < NUM_PE ; i++){
+        for (int j = 0; j < NUM_P; j++){
+          tot_conflict[j] |= (cls_ded[i][j] == -1 ? 0 : 1<<i); 
+        }
       }
-    }
       
-    is_ded[0] = 0;
-    is_ded[1] = 1;
-    is_ded[2] = 2;
-    is_ded[3] = 3;
-    #pragma ACCEL parallel flatten reduction=is_ded
-    for (int i = 0; i < NUM_PE ; i++){
-      for (int j = 0; j < NUM_P; j++){
-        is_ded[j] |= (cls_ded[i][j] == -1 ? 0 : 1<<i); 
+      is_ded[0] = 0;
+      is_ded[1] = 1;
+      is_ded[2] = 2;
+      is_ded[3] = 3;
+      #pragma ACCEL parallel flatten reduction=is_ded
+      for (int i = 0; i < NUM_PE ; i++){
+        for (int j = 0; j < NUM_P; j++){
+          is_ded[j] |= (cls_ded[i][j] == -1 ? 0 : 1<<i); 
+        }
       }
-    }
 
-    conf_p = tot_conflict[0] != 0 ? 0 : 
+      conf_p = tot_conflict[0] != 0 ? 0 : 
             tot_conflict[1] != 0 ? 1 :
             tot_conflict[2] != 0 ? 2 :
             tot_conflict[3] != 0 ? 3 : -1;
 
-    if (conf_p != -1){
+      if (conf_p != -1){
         conf_pe = priority_encoder_16(tot_conflict[conf_p]); 
         conf_var = prop_var;
         conf_cls = (pid_cls_info[conf_pe][prop_var][conf_p] >> 6) & conf_pe;
         break; 
-    }
+      }
 
-    for (int x = 0; x < NUM_P; x++){
-      ded_pe = priority_encoder_64(is_ded[x]);
-      while (ded_pe != -1){
-        int var_ded = abs(l_ded[ded_pe][x]);
-        int var_ded_value = (l_ded[ded_pe][x] == POS) ? T : F; 
-        if (var_truth_table[var_ded] == U){
-          buf_ded_end ++;
-          buf_ded_lit[buf_ded_end] = l_ded[ded_pe][x]; 
-          buf_ded_parent_cls[buf_ded_end] =  (cls_ded[ded_pe][x] >> 6 )& ded_pe; 
-          printf("Add ded var %d due to cls %d\n", buf_ded_lit[buf_ded_end], buf_ded_parent_cls[buf_ded_end]);
-          //Change ded value here
-          var_truth_table[var_ded] = l_ded[ded_pe][x] > 0 ? T : F;
-        }else if (var_truth_table[var_ded] != var_ded_value){
-          //Found conflict in same level
-          conf_p=1; 
-          conf_var = var_ded;
-          conf_cls = cls_ded[ded_pe][x] & (ded_pe >> 16);
-          printf("Found inner conflict Var(%d) due to cls(%d)\n", conf_var, conf_cls);
+      for (int x = 0; x < NUM_P; x++){
+        ded_pe = priority_encoder_64(is_ded[x]);
+        while (ded_pe != -1){
+          int var_ded = abs(l_ded[ded_pe][x]);
+          int var_ded_value = (l_ded[ded_pe][x] == POS) ? T : F; 
+          if (var_truth_table[var_ded] == U){
+            buf_ded_end ++;
+            buf_ded_lit[buf_ded_end] = l_ded[ded_pe][x]; 
+            buf_ded_parent_cls[buf_ded_end] =  (cls_ded[ded_pe][x] >> 6 )& ded_pe; 
+            printf("Add ded var %d due to cls %d\n", buf_ded_lit[buf_ded_end], buf_ded_parent_cls[buf_ded_end]);
+            //Change ded value here
+            var_truth_table[var_ded] = l_ded[ded_pe][x] > 0 ? T : F;
+          }else if (var_truth_table[var_ded] != var_ded_value){
+            //Found conflict in same level
+            conf_p=1; 
+            conf_var = var_ded;
+            conf_cls = cls_ded[ded_pe][x] & (ded_pe >> 16);
+            printf("Found inner conflict Var(%d) due to cls(%d)\n", conf_var, conf_cls);
+          }
         }
+        is_ded[x] ^= (1 << ded_pe);
       }
-      is_ded[x] ^= (1 << ded_pe);
-    }
 
-    assert (buf_ded_end < BUF_DED_SIZE);
+      assert (buf_ded_end < BUF_DED_SIZE);
 
-    if (conf_p){
-      //Found conflict 
-      isdone = 1;
-    }else if (buf_ded_curr == buf_ded_end){
-      //No deducted variable in buf_ded
-      isdone = 1;
-    }else{
-      //Move to next variable in buf_ded
-      buf_ded_curr++;
-      prop_var = buf_ded_lit[buf_ded_curr];
-    }
-
-    }//End of isdone while loop
-
-
-    int conf_info_out = conf_p | conf_var << 1 |  conf_cls << 9;
-    int assigned_value = 0; 
-    int var_idx_out1 = 0; 
-    int var_idx_out2 = 0; 
-
-    #pragma ACCEL parallel flatten reduction=assigned_value
-    for (int i = 0; i < BUF_DED_SIZE; i++){
-      if (i <= buf_ded_end){
-        assigned_value |= (var_truth_table[abs(buf_ded_lit[i])] << i); 
+      if (conf_p){
+        //Found conflict 
+        state = BACKTRACK_DEC; 
+        buf_ded_curr = -1; buf_ded_end = -1;
+      }else if (buf_ded_curr == buf_ded_end){
+        //No deducted variable in buf_ded
+        state = DECISION;
+        buf_ded_curr = -1; buf_ded_end = -1;
+      }else{
+        //Move to next variable in buf_ded
+        buf_ded_curr++;
+        state = PROP; 
       }
-    }
+      break;
 
+    case BACKTRACK_DEC: 
+      back_lvl = curr_lvl; 
+      prev_state = BACKTRACK_DEC;
 
-    for (int i = 0; i < BUF_DED_SIZE; i++){
-      if (i <= buf_ded_end){
-        if (i < 8){
-          var_idx_out1 |= (abs(buf_ded_lit[i])) << (8*i);  
-        }else {
-          var_idx_out2 |= (abs(buf_ded_lit[i])) << (8*(i-1));  
+      if (back_lvl < 0){
+        printf("Failed at lvl %d\n", back_lvl);
+        state = FAILED; 
+        break;
+      }
+
+      printf("Back to lvl %d - Var %d\n", back_lvl, dec_var[back_lvl]);
+
+      prev_assigned_value = var_truth_table[dec_var[back_lvl]]; 
+        //Undo all variable assignment after back_lvl
+        #pragma ACCEL parallel flatten
+        for (int i = 0; i < NUM_VARS; i ++){
+          if (dec_lvl[i] >= back_lvl){
+            var_truth_table[i] = U;
+            dec_lvl[i] = -1; 
+            parent_cls[i] = -1;
+          }
         }
-      }
-    }
 
-    data_out1[0] = conf_info_out;
-    data_out1[1] = assigned_value | (buf_ded_end << 16); 
-    data_out2[0] = var_idx_out1;
-    data_out2[1] = var_idx_out2;
+        #pragma ACCEL parallel flatten
+        for (int i = 0; i < BUF_DEC_LVL; i++){
+          if (i > back_lvl){
+            dec_var[i] = 0; 
+          }
+        }
 
-    data_out3[0] = ((buf_ded_end >= 0) ? buf_ded_parent_cls[0] : 0) | 
-                  ((buf_ded_end >= 1) ? buf_ded_parent_cls[1] << 16: 0) |
-                  ((buf_ded_end >= 2) ? buf_ded_parent_cls[2] << 32: 0) |
-                  ((buf_ded_end >= 3) ? buf_ded_parent_cls[1] << 48: 0) ; 
+        new_var_idx = dec_var[back_lvl]; 
+        var_truth_table[new_var_idx] = (prev_assigned_value == T) ? F : T;
+        dec_lvl[new_var_idx] = back_lvl;
+        //least_parent[new_var_idx] = new_var_idx;
+        //printf("Change VTT Var(%d) to %d\n", dec_var[back_lvl], var_truth_table[dec_var[back_lvl]]);
+        curr_lvl = back_lvl; 
 
-    data_out3[1] =  ((buf_ded_end >= 4) ? buf_ded_parent_cls[4] : 0) | 
-                  ((buf_ded_end >= 5) ? buf_ded_parent_cls[5] << 16: 0) |
-                  ((buf_ded_end >= 6) ? buf_ded_parent_cls[6] << 32: 0) |
-                  ((buf_ded_end >= 7) ? buf_ded_parent_cls[7] << 48: 0) ; 
+        state = PROP;
+        break;
+     case SOLVED:
+        printf("Solved\n");
+        /*
+        for (int i = 0; i < NUM_VARS; i++){
+            result[i] = var_truth_table[i];
+        }*/
+        result[0] = 1;
+        state = EXIT;
+        break; 
 
-    data_out4[0] =  ((buf_ded_end >= 8) ? buf_ded_parent_cls[8] : 0) | 
-                  ((buf_ded_end >= 9) ? buf_ded_parent_cls[9] << 16: 0) |
-                  ((buf_ded_end >= 10) ? buf_ded_parent_cls[10] << 32: 0) |
-                  ((buf_ded_end >= 11) ? buf_ded_parent_cls[11] << 48: 0) ; 
-
-    data_out4[0] =  ((buf_ded_end >= 12) ? buf_ded_parent_cls[12] : 0) | 
-                  ((buf_ded_end >= 13) ? buf_ded_parent_cls[13] << 16: 0) |
-                  ((buf_ded_end >= 14) ? buf_ded_parent_cls[14] << 32: 0) |
-                  ((buf_ded_end >= 15) ? buf_ded_parent_cls[15] << 48: 0) ;              
-
-  }//End of mode DEC
-
-  /*
-       *        data_out1[0] = {conf_cls_no, conf_cls_pe, conf_var, conf} // conf_cls_no(10), conf_cls_pe(6), conf_var(8), conf(1)
-       *        data_out1[1] =  {num of ded, xxxx xxxx xxxx xxxx} //assigned value for the above data , num of ded variable
-       *        data_out2[0/1] = {var_idx*8} //Each 8 bits - total 8*2 variables
-       *        data_out3[0/1] = {parent_cls1*4} //Each parent_cls has cls_no(10) & pe_no(6)
-       *        data_out4[0/1] = {parent_cls1*4} //Each parent_cls has cls_no(10) & pe_no(6)
-       *  BACK: Same as above   
-       *        data_in2 = {back_lvl}   
-     */
-
-  
-
-  
-
-      ii++;
-    printf("Check %d\n", ii);
+      case FAILED:
+        printf("Failed to solve the problem. \n");
+        result[0] = 0;
+        state = EXIT; 
+        break;
+      }//End of sw
+    }//End of while
 
       
 
